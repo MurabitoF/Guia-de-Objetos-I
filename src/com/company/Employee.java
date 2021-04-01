4fa30123c1fb94fce9bdaf0e78c03766979587c6
MurabitoF <66337385+MurabitoF@users.noreply.github.com>
2021-04-01 18:17:34 -0300

    Clase Employee y ejercicio 2

---
+++ src/com/company/Employee.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Employee {
+    private String name;
+    private String lastName;
+    private String dni;
+    private Double salary;
+
+    public Employee(String name, String lastName, String dni, Double salary) {
+        this.name = name;
+        this.lastName = lastName;
+        this.dni = dni;
+        this.salary = salary;
+    }
+
+    public void showEmployee(){
+        //System.out.println("Employee[dni=" + this.dni + ", name=" + this.name + ", last name=" + this.lastName + ", salary=" + this.salary + " ]");
+        String output = String.format("Employee[dni=%s, name=%s, lastName=%s, salary=%1.2f]", this.dni, this.name, this.lastName, this.salary);
+        System.out.println(output);
+    }
+
+    public double getAnnualSalary(){
+        return 12 * this.salary;
+    }
+
+    public void increaseSalary(double increase){
+        this.salary = (double)Math.round((1 + (increase/100)) * this.salary * 100)/100;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
         //ej1();
+        ej2();
     }
 
-    static void ej1(){
+    public static void ej1(){
         Rectangle rectangle1 = new Rectangle(5.0, 3.6);
         Scanner scan = new Scanner(System.in);
 
@@ -30,6 +31,18 @@ public class Main {
         System.out.println("Width: " + rectangle2.getWidth() + "   Height: " + rectangle2.getHeight());
         System.out.println("Area: " + rectangle2.getArea());
         System.out.println("Perimeter: " + rectangle2.getPerimeter());
+    }
+
+    public static void ej2(){
+        Employee employee1 = new Employee("Carlos", "Gutiérrez", "23456345", 25000.00);
+        Employee employee2 = new Employee("Ana", "Sánchez", "34234123", 27500.00);
+
+        employee1.showEmployee();
+        employee2.showEmployee();
+
+        employee1.increaseSalary(15);
+        System.out.println(employee1.getAnnualSalary());
+
     }
 
 }
